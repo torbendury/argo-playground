a1c7211ae1fdc1cca1ef33b51653601e99606fe6
Torben Dury <torben.dury@sva.de>
2026-03-24 13:39:15 +0100

    delete k8s resources on app deletion

--- upgrade.sh
+++ upgrade.sh
@@ -6,15 +6,15 @@ new_ver=$1
 
 echo "new version: $new_ver"
 
-docker tag docker.io/library/nginx:latest docker.io/torbendury/nginx:$new_ver
-docker push docker.io/torbendury/nginx:$new_ver
+podman tag docker.io/library/nginx:latest docker.io/torbendury/nginx:$new_ver
+podman push docker.io/torbendury/nginx:$new_ver
 
 tmp_dir=$(mktemp -d)
 echo $tmp_dir
 
-git clone github.com/torbendury/argo-playground.git $tmp_dir
+git clone git@github.com:torbendury/argo-playground.git $tmp_dir
 
-sed -i '' -e "s/torbendury\/nginx:.*/torbendury\/nginx:$new_ver/" $tmp_dir/my-app/1_deployment.yaml
+sed -i -e "s/torbendury\/nginx:.*/torbendury\/nginx:$new_ver/g" "$tmp_dir/my-app/1_deployment.yaml"
 
 cd $tmp_dir
 git add .
