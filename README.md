e09c34735e01bef63876a587c65a8153cd68557f
Torben Dury <torben.dury@sva.de>
2026-03-24 11:03:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ArgoCD playground repo
+
+🚧🚧🚧 Construction only! 🚧🚧🚧
+
+Playground repository made for tinkering around with ArgoCD. Deployed locally to minikube via Terraform.
---
+++ terraform/1_argocd.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "argocd" {
+  name = "argocd"
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd" # default by argo
+
+  values = [file("values/argocd.yaml")]
+}
